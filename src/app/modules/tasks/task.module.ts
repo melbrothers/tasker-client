c5f9cc3c33e4285a5f41065431e71d389e40802e
Lixing <zlxjackie@hotmail.com>
2019-02-02 22:57:19 +1100

    Fix task

--- src/app/modules/tasks/task.module.ts
+++ src/app/modules/tasks/task.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { TaskListComponent } from 'app/modules/tasks/task-list/task-list.component';
 import { TaskRoutingModule } from 'app/modules/tasks/task-routing.module';
 import {TaskComponent} from './task/task.component';
+import { TaskService } from 'app/core/services/task.service';
+import { TaskDataResolver } from 'app/modules/tasks/tasks-resolver.service';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,10 @@ import {TaskComponent} from './task/task.component';
   imports: [
     CommonModule,
     TaskRoutingModule
+  ],
+  providers: [
+    TaskService,
+    TaskDataResolver
   ],
   exports: [
     TaskListComponent
--- src/app/modules/tasks/task/task.component.ts
+++ src/app/modules/tasks/task/task.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { Task } from 'app/store/models/task.model';
 
 @Component({
   selector: 'app-task',
@@ -6,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-  @Input() task: any;
+  @Input() task: Task;
   constructor() { }
 
   ngOnInit() {
