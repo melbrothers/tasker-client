8144a5af646d51e2092b73d0f84baa8761ae009d
Zheng <jason.zhengxin@gmail.com>
2019-02-06 21:01:01 +1100

    task detail page ui update

--- src/app/modules/tasks/task/task.component.ts
+++ src/app/modules/tasks/task/task.component.ts
@@ -4,7 +4,7 @@ import { Task } from 'app/store/models/task.model';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.scss']
 })
 export class TaskComponent implements OnInit {
   @Input() task: Task;
