59f13017f89add76f323c26fcfa52bd1eaef02cb
Lixing Zhang <zlxjackie@hotmail.com>
2019-02-08 11:58:16 +1100

    Fix https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://taskerapi.herokuapp.com',
+  apiUrl: 'https://taskerapi.herokuapp.com',
   // apiUrl: 'http://localhost:8000',
   googleClientId: '611463719645-cvunnfjflkvike7abo38vogvjb0uapbp.apps.googleusercontent.com'
 };
