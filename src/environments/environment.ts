588f424d05a3fa932f38ae6f15d9e3b74f005fd4
Zheng <jason.zhengxin@gmail.com>
2019-02-14 23:42:14 +1100

    recover env" ;

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://taskerapi.herokuapp.com',
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://taskerapi.herokuapp.com',
+  // apiUrl: 'http://localhost:8000',
   googleClientId: '611463719645-cvunnfjflkvike7abo38vogvjb0uapbp.apps.googleusercontent.com',
   googlePlacesAPIKey: 'AIzaSyAE861yD_u4Ig-0tpYXyJqJeXxI9QljXNo',
   googleMapsAPIKey: 'AIzaSyDmiPoETLsE4NsEQX-8suRYAlaOr2Z7790'
