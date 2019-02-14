7009e5f42a566775dbd95c8ba11a9513f6ec3370
Zheng <jason.zhengxin@gmail.com>
2019-02-14 23:41:31 +1100

    post task ui update

--- src/app/modules/account/post-task/post-task.component.ts
+++ src/app/modules/account/post-task/post-task.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {TheErrorStateMatcher} from '../../../core/utils/errorMatcher';
+import {Coordinates} from '../../../store/models/coordinates.model';
+import {GooglePlaceService} from '../../../core/services/google-place.service';
+import {MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-task',
@@ -11,6 +15,7 @@ export class PostTaskComponent implements OnInit {
   isLinear = true;
   taskForm: FormGroup;
   matcher = new TheErrorStateMatcher();
+  coordinates: Coordinates;
   selectedSuburb = {
     display_name: null,
     longitude: 0,
@@ -18,7 +23,25 @@ export class PostTaskComponent implements OnInit {
   };
   deadline = new Date();
   isCertainDay = false;
-  constructor(private _formBuilder: FormBuilder) {
+  morning = false;
+  midday = false;
+  afternoon = false;
+  evening = false;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _googlePlaceService: GooglePlaceService,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'sunrise-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/lower-sun.svg'));
+    iconRegistry.addSvgIcon(
+      'sunset-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/sunset.svg'));
+    iconRegistry.addSvgIcon(
+      'moon-icon',
+      sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/moon.svg'));
     this.createForm();
   }
   onAutocompleteSelected(ev) {
@@ -35,6 +58,19 @@ export class PostTaskComponent implements OnInit {
     }
   }
   ngOnInit(): void {
+    // get current location
+    // TODO: currently temporarily does not need this
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(location => {
+    //     console.log(location);
+    //     if (location && location.coords) {
+    //       this.coordinates = new Coordinates(location.coords.latitude, location.coords.longitude);
+    //       this._googlePlaceService.getAddressFromLatLang(this.coordinates).subscribe(res => {
+    //         console.log(res);
+    //       });
+    //     }
+    //   });
+    // }
   }
 
   createForm() {
