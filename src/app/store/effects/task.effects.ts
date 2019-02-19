a1758ca45dd1d73d1bc5b9dff44bd2a5ad2547c4
Zheng <jason.zhengxin@gmail.com>
2019-02-19 23:45:08 +1100

    apply store entities for tasks

--- src/app/store/effects/task.effects.ts
+++ src/app/store/effects/task.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Actions, Effect, ofType} from '@ngrx/effects';
-import { RequestTask, SelectTask, TaskActionTypes} from '../actions/task.actions';
+import {LoadTasks, RequestTask, SelectTask, TaskActionTypes} from '../actions/task.actions';
 import {map, mergeMap} from 'rxjs/operators';
 import {TaskService} from '../../core/services/task.service';
 
@@ -8,9 +8,17 @@ import {TaskService} from '../../core/services/task.service';
 export class TaskEffects {
   constructor(private actions$: Actions, private taskService: TaskService) {}
   @Effect()
-  loadTasks$ = this.actions$.pipe(
+  getTask$ = this.actions$.pipe(
     ofType<SelectTask>(TaskActionTypes.SELECT_TASK),
     mergeMap(action => this.taskService.getTask(action.payload.task.slug)),
     map(task => new SelectTask({task})),
   );
+
+  @Effect()
+  loadTasks$ = this.actions$.pipe(
+    ofType<LoadTasks>(TaskActionTypes.LOAD_TASKS),
+    mergeMap(action => this.taskService.listTasks()),
+    map(tasks => new LoadTasks({tasks}))
+  );
+
 }
